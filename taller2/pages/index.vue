ad5c764238f5ed574aada1fe183b322099a8742a
AldesQuintero <67674457+AldesQuintero@users.noreply.github.com>
2021-12-22 19:54:30 -0500

    Taller #2

---
+++ taller2/pages/index.vue
@@ -0,0 +1,85 @@
+<template>
+      
+    <div > 
+    <h1> Programación Gráfica</h1> 
+    <div class="rcorners center ">
+    <div class="container" style="text-align:center">
+        <div class="title" >
+        <h1>Taller #2</h1>
+        </div>
+ <img src="/img/computer2.png" alt="Forwarder Genius" width="250pt">
+        <br>
+        <h2>Aldes Quintero</h2>
+
+  <div class="form-group">
+   
+  </div>
+
+  <button href="http://hwfircdtdqvnpcc4agkmwq-on.drv.tw/web/aldesquintero.html" class="btn mainbut btn-block">Presentación</button>
+
+
+    </div>
+
+    </div>
+    </div>
+
+
+</template>
+
+
+<script>
+ 
+</script>
+
+<style scoped>
+
+
+.title{
+    box-sizing: border-box;
+    border: 2px solid rgb(136, 199, 231);
+    font-family: 'Arial', sans-serif;
+    background-color: #4caef5;
+    color: rgb(69, 98, 113);
+    padding: 1rem 2rem;
+    border-radius: 20px;
+    display: inline-block;
+   
+}
+
+
+.rcorners {
+  border-radius: 25px;
+  border: 1px solid #1a82a8;
+  padding: 20px;
+  width: 430px;
+  background-color: rgb(255, 255, 255);
+  
+}
+.btText{
+    font-size: 10px;
+}
+.mainbut{
+ background-color:#93a8ba;
+ font-size: 28px;
+ color: white;
+ padding: 1rem 2rem;
+border-radius: 10px;
+}
+
+h2 {
+    color: #65747b;
+}
+
+h1 {
+    color: #697c85;
+}
+.center{
+ position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+
+  
+}
+</style>
